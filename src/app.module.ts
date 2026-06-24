94bd6a2903fc83bb67fe0b71641a215d0ae571e5
MarlonM001 <stimonl06@gmial.>
2026-06-24 15:40:45 -0500

    se genera modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RolesModule } from './modules/roles/roles.module';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
+import { SeguidoresModule } from './modules/seguidores/seguidores.modules';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     RolesModule,
     UsuariosModule,
+    SeguidoresModule,
     MongooseModule.forRoot(process.env.MONGO_URI as string),
   ],
   controllers: [AppController],
